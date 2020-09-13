c0f253fbb5d48ab1335f1982980f4630b703d885
danielitogomez <daniel.gomez.amaran@gmail.com>
2020-09-13 00:13:19 -0300

    fix files

--- providers.tf
+++ providers.tf
@@ -1,9 +1,8 @@
 provider "aws" {
   region = var.aws_reg
-  # version = "2.12.0"
   profile = "terraform"
-  access_key = "AKIAY24J45TV6UIABRVS"
-  secret_key = "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"
+  access_key = "AKIAVI2K6HJKVQVZYIU7"
+  secret_key = "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"
 }
 
 
